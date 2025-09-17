e691b09a689582139d46740f77dd3295f731d476
KundanPatel <kppatel43@myseneca.ca>
2025-09-16 21:59:42 -0400

    Create basic Express server with root endpoint

--- server.js
+++ server.js
@@ -10,3 +10,15 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("API running on http://localhost:3000");
 });
+
+
+// Sample user list
+let users = [
+  { id: 1, username: "alice" },
+  { id: 2, username: "bob" }
+];
+
+// GET /users endpoint
+app.get("/users", (req, res) => {
+  res.json(users);
+});
